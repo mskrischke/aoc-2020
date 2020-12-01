6101b52b04958101fb7e521aba8ef258ab7fb3a3
Markus Krischke <markus.krischke@gmail.com>
2020-12-01 13:00:20 +0100

    Initial Commit

---
+++ aoc-2020/aoc_2020App.swift
@@ -0,0 +1,17 @@
+//
+//  aoc_2020App.swift
+//  aoc-2020
+//
+//  Created by Markus Krischke on 01.12.20.
+//
+
+import SwiftUI
+
+@main
+struct aoc_2020App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
